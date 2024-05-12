f2e96d5334c23acd81641fe55ef5df8bd6dd1c39
aakashverma <werma.official@gmail.com>
2024-05-12 15:10:56 +0530

    After testing the prediction using the Flask API

--- app.py
+++ app.py
@@ -22,6 +22,18 @@ def predict_api():
     output=regression.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods = ['POST'])
+
+def predict():
+    data=[float(x) for x in request.form.values()]
+    input = scalar.transform(np.array(data).reshape(1,-1))
+    print(input)
+    output = regression.predict(input)[0]
+    return render_template("home.html", prediction_text="predction is {}".format(output))
+
+
+
 if __name__ =="__main__":
     app.run(debug=True)
 
